1d6d83b43737f4983d6c83376742d150b86019af
Leonel Menaia <leonel.menaia@ongagement.com>
2019-03-09 12:36:33 +0000

    remove: views

---
+++ controllers/PhoneController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use app\common\controllers\BaseController;
+
+class PhoneController extends BaseController
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionTest()
+    {
+        return $this->response->success();
+    }
+}
