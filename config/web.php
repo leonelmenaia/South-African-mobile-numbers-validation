3fb549ef561d12d116b2b422f6be995caa816d06
Leonel Menaia <leonel.menaia@ongagement.com>
2019-03-21 21:42:45 +0000

    add: v1 module

--- config/web.php
+++ config/web.php
@@ -77,7 +77,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['*'],
+        'allowedIPs' => ['*'],
     ];
 }
 
---
+++ modules/v1/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\modules\v1;
+
+
+use yii\base\Module;
+
+class Campaign extends Module
+{
+
+    public $controllerNamespace = 'app\modules\v1\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+
+}
